99f0bb26af77fdb3603e728373ef5feabc431144
flyactiv <flyactiv46@gmail.com>
2021-04-19 16:18:47 +0300

    Added first dependencies

---
+++ ocharick-mvc-web/src/main/java/com/example/ocharickmvcweb/OcharickMvcWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.ocharickmvcweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcharickMvcWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcharickMvcWebApplication.class, args);
+	}
+
+}
---
+++ ocharick-mvc-web/src/test/java/com/example/ocharickmvcweb/OcharickMvcWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ocharickmvcweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcharickMvcWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

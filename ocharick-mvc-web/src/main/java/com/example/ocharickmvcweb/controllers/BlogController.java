8d0f74c4cde5833b7d8505b1df7becb6186df025
flyactiv <flyactiv46@gmail.com>
2021-04-20 13:52:01 +0300

    Added a database for blog page

--- ocharick-mvc-web/src/main/java/com/example/ocharickmvcweb/controllers/BlogController.java
+++ ocharick-mvc-web/src/main/java/com/example/ocharickmvcweb/controllers/BlogController.java
@@ -1,14 +1,25 @@
 package com.example.ocharickmvcweb.controllers;
 
+import com.example.ocharickmvcweb.models.AllPost;
+import com.example.ocharickmvcweb.repo.AllPostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BlogController {
+
+    @Autowired
+    private AllPostRepository allPostRepository;
+
     @GetMapping("/about")       //Контроллер для отслеживания url страницы блога web-ресурса
     public String blogMain(Model model) {
         model.addAttribute("title", "Страница блога");
+        Iterable<AllPost> allPosts = allPostRepository.findAll();
+        model.addAttribute("allPosts", allPosts);
+
         return "about";
     }
+
 }
---
+++ ocharick-mvc-web/src/main/java/com/example/ocharickmvcweb/repo/AllPostRepository.java
@@ -0,0 +1,9 @@
+package com.example.ocharickmvcweb.repo;
+
+import com.example.ocharickmvcweb.models.AllPost;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AllPostRepository extends CrudRepository<AllPost, Long> {
+
+
+}

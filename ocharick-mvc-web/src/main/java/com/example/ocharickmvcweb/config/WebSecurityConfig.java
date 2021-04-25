9c128e762082af780481a55f0ca4358d9ec76754
flyactiv <flyactiv46@gmail.com>
2021-04-25 23:07:31 +0300

    Corrected the first part of the report

---
+++ ocharick-mvc-web/src/main/java/com/example/ocharickmvcweb/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.ocharickmvcweb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("Admin")
+                .password("Admin")
+                .roles("ADMIN");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/addPost").hasRole("ADMIN")
+                .antMatchers("/about/{id}/edit").hasRole("ADMIN")
+                .antMatchers("redirect:/about").permitAll()
+                .and().formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}

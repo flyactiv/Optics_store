1d247e270887adb21d18a5b3e743810a8c555879
flyactiv <flyactiv46@gmail.com>
2021-05-19 19:37:59 +0300

    Added the role of admin and user and prohibited the transition to some pages

--- ocharick-mvc-web/src/main/java/com/example/ocharickmvcweb/controllers/ContactsController.java
+++ ocharick-mvc-web/src/main/java/com/example/ocharickmvcweb/controllers/ContactsController.java
@@ -3,6 +3,8 @@ package com.example.ocharickmvcweb.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class ContactsController {
@@ -11,4 +13,5 @@ public class ContactsController {
         model.addAttribute("title", "Страница контактов");
         return "contact";
     }
+
 }
